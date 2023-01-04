4563f4a53d83d06b732fa16f37df7b6c7886d215
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-01-04 16:47:04 -0400

    Config Routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,15 +2,13 @@ import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    return res.json({message: 'Hello Dev'})
-})
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando no http://localhost:${process.env.PORT}`)
