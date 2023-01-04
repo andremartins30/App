3ab9d17ecd966f73e9ae5ee57615bd334c9a30bb
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-01-04 16:55:23 -0400

    throw new error

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 const routes = Router()
 
 routes.get('/', (req, res) => {
+    throw new Error( 'Acesso negado!')
     return res.json({message: 'Hello Dev'})
 })
 
