214da163eb045cb1b90cd7180d45144532516a11
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-01-04 21:22:28 -0400

    Add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,10 @@
+import { AppError } from "@shared/errors/AppError";
 import { Router } from "express";
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
-    throw new Error( 'Acesso negado!')
+    throw new AppError('Acesso negado')
     return res.json({message: 'Hello Dev'})
 })
 
