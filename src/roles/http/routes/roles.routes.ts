2820b0252b98a9b4025352bfdc7f8a6e84f76896
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-01-10 00:44:20 -0400

    Add FindAll

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -9,9 +9,14 @@ const rolesRepository = new RolesRepository()
 rolesRouter.post('/', (req, res) => {
     const {name} = req.body
     const role = rolesRepository.create({ name })
-
-
     return res.status(201).json(role)
+
+})
+
+rolesRouter.get('/', (req, res) => {
+
+    const roles = rolesRepository.findAll()
+    return res.json(roles)
 
 })
 
